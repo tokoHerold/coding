1b1b936ae6271d5d469860e398609129e2d82011
tokoHerold <wombatwheelie@gmail.com>
2025-01-30 14:33:55 +0100

    Make script loop 4 times for better results

--- thermal/scripts/measure_cache.bash
+++ thermal/scripts/measure_cache.bash
@@ -6,19 +6,23 @@ NCPUS=8
 make cache CFLAGS="-O0 -DMILLIS=2"
 mkdir -p data
 
-echo "Measuring cache-aware program..."
-taskset --cpu-list $((NCPUS - 1)) ./out/good_cache &
-bPID=$!
-./out/temp/ $HWMON > data/good_cache.txt
-kill $bPID
-echo ...done!
-echo Cooling down...
+for i in $(seq 1 4);
+do
+    echo "Measuring cache-aware program..."
+    taskset --cpu-list $((NCPUS - 1)) ./out/good_cache &
+    bPID=$!
+    ./out/temp $HWMON > data/good_cache_$i.txt
+    kill $bPID
+    echo ...done!
+    echo Cooling down...
 
-sleep 60
+    sleep 3
 
-echo "Measuring non-cache-aware program..."
-taskset --cpu-list $((NCPUS - 1)) ./out/bad_cache &
-bPID=$!
-./out/temp/ $HWMON > data/bad_cache.txt
-kill $bPID
-echo ...done!
+    echo "Measuring non-cache-aware program..."
+    taskset --cpu-list $((NCPUS - 1)) ./out/bad_cache &
+    bPID=$!
+    ./out/temp $HWMON > data/bad_cache_$i.txt
+    kill $bPID
+    echo ...done!
+    sleep 3
+done
