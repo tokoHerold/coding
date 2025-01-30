7ad44719607826c6543cd2176a1767235ad561d9
tokoHerold <wombatwheelie@gmail.com>
2025-01-30 14:22:01 +0100

    Add script to measure cache-temperatures

---
+++ thermal/scripts/measure_cache.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+HWMON=/sys/class/hwmon/hwmon1/temp*_input
+NCPUS=8
+
+make cache CFLAGS="-O0 -DMILLIS=2"
+mkdir -p data
+
+echo "Measuring cache-aware program..."
+taskset --cpu-list $((NCPUS - 1)) ./out/good_cache
+bPID=$!
+./out/temp/ $HWMON > data/good_cache.txt
+kill $bPID
+echo ...done!
+echo Cooling down...
+
+sleep 60
+
+echo "Measuring non-cache-aware program..."
+taskset --cpu-list $((NCPUS - 1)) ./out/bad_cache
+bPID=$!
+./out/temp/ $HWMON > data/bad_cache.txt
+kill $bPID
+echo ...done!
