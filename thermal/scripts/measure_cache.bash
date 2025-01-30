49f414a6109aec9fc72a117464a95588c30a1523
tokoHerold <wombatwheelie@gmail.com>
2025-01-30 14:27:21 +0100

    Add missing *

--- thermal/scripts/measure_cache.bash
+++ thermal/scripts/measure_cache.bash
@@ -7,7 +7,7 @@ make cache CFLAGS="-O0 -DMILLIS=2"
 mkdir -p data
 
 echo "Measuring cache-aware program..."
-taskset --cpu-list $((NCPUS - 1)) ./out/good_cache
+taskset --cpu-list $((NCPUS - 1)) ./out/good_cache &
 bPID=$!
 ./out/temp/ $HWMON > data/good_cache.txt
 kill $bPID
