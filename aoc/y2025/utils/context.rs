1504b9b8ac087ad85df877ff70dad4df9af0811d
tokoHerold <wombatwheelie@gmail.com>
2025-12-08 00:15:22 -0800

    Make context take additional number param

--- aoc/y2025/utils/context.rs
+++ aoc/y2025/utils/context.rs
@@ -6,6 +6,7 @@ static GLOBAL_CONTEXT: OnceLock<Context> = OnceLock::new();
 pub struct Context {
     filepath: String,
     verbose: bool,
+    number: Option<i32>,
 }
 
 impl Context {
@@ -17,23 +18,42 @@ impl Context {
     /// if it receives invalid command line parameters.
     fn new() -> Self {
         let args: Vec<String> = env::args().collect();
-        if args.len() < 2 || args.len() > 3 {
-            eprintln!("Usage: {} <path-to-file> [-v]", &args[0]);
-            process::exit(1)
-        }
-        if args.len() == 3 && args[1] != "-v" && args[2] != "-v" {
-            eprintln!("Usage: {} <path-to-file> [-v]", &args[0]);
-        }
-        if args[1] == "-v" {
-            Context {
-                filepath: args[2].clone(),
-                verbose: true,
+        let mut filepath = String::new();
+        let mut verbose = false;
+        let mut number = None;
+        let mut iter = args.iter().skip(1);
+
+        while let Some(arg) = iter.next() {
+            match arg.as_str() {
+                "-v" => verbose = true,
+                "-n" => {
+                    if let Some(next_arg) = iter.next() {
+                        if let Ok(num) = next_arg.parse::<i32>() {
+                            number = Some(num);
+                            continue;
+                        } else {
+                            eprintln!("Invalid number provided after -n");
+                            process::exit(-1);
+                        }
+                    } else {
+                        eprintln!("Expected a number after -n");
+                        process::exit(-1);
+                    }
+                }
+                _ => {
+                    if filepath.is_empty() {
+                        filepath = arg.clone();
+                    } else {
+                        eprintln!("Unexpected argument: {}", arg);
+                        process::exit(-1);
+                    }
+                }
             }
-        } else {
-            Context {
-                filepath: args[1].clone(),
-                verbose: if args.len() == 2 { false } else { true },
-            }
+        }
+        Self {
+            filepath,
+            verbose,
+            number,
         }
     }
     /// Reads and returns all lines of the file specified in the context struct.
@@ -50,6 +70,15 @@ impl Context {
             }
             Ok(str) => str,
         }
+    }
+
+    pub fn get_number() -> i32 {
+        let num = Self::get().number;
+        if num.is_none() {
+            eprintln!("Error: Missing number parameter '-n <number>'");
+            process::exit(-1);
+        }
+        num.unwrap()
     }
 
     pub fn is_verbose(&self) -> bool {
